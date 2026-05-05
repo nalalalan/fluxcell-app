67f36e5e9503bf8a1ffeef5590db05ba06384209
nalalalan <pham.alann@gmail.com>
2026-05-05 13:49:28 -0400

    Point FluxCell to Railway AI backend

--- public/config.js
+++ public/config.js
@@ -1,2 +1,2 @@
 window.FLUXCELL_API_BASE = window.FLUXCELL_API_BASE || "";
-window.FLUXCELL_AI_API_BASE = window.FLUXCELL_AI_API_BASE || "";
+window.FLUXCELL_AI_API_BASE = window.FLUXCELL_AI_API_BASE || "https://fluxcell-api-production.up.railway.app";
